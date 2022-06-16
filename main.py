5cbd38531e4faa4a04231769950c19157a166a13
Jacob Baglione <103280951+binijr08@users.noreply.github.com>
2022-06-16 01:41:26 +0000

    Completed

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 """
-Name(s):
-Name of Project:
+Name(s): Silas Smith, Jacob Baglione
+Name of Project: Rock Paper Scissors Best of Three
 """
 
-#Write the main part of your program here. Use of the other pages is optional.
-
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+import random
+options=['Rock','Paper','Scissors']
+name=input("Enter your name: ")
+ComputerScore=0 
+PlayerScore=0
+NumberOfRounds=0
+gameOn=True
+print(f"Welcome {name.title()}")
+while NumberOfRounds<3:
+  ComputerOption=random.choice(options)
+  PlayerOption=input("Enter Rock/ Paper/ Scissors: ").title()
+  print(f"Computer option: {ComputerOption}")
+  print(f"{name.title()} option: {PlayerOption}")
+  NumberOfRounds += 1
+  if ComputerOption==PlayerOption:
+    print('Tie')
+  elif (ComputerOption=='Rock' and PlayerOption == 'Scissors') or (ComputerOption=='Scissors' and PlayerOption=='Paper') or (ComputerOption=='Paper' and PlayerOption=='Rock'):
+    print("Computer wins")
+    ComputerScore += 1
+  elif (PlayerOption=='Rock' and ComputerOption == 'Scissors') or (PlayerOption=='Scissors' and ComputerOption=='Paper') or (PlayerOption=='Paper' and ComputerOption=='Rock'):
+    print(f"{name.title()} wins")
+    PlayerScore += 1
+  else:
+    print("Trolololol noone wins.") 
+#its not a bug its a feature LOL
+  print("-------------------------")
+  print("")
+  print(f"Round No: {NumberOfRounds}")
+  print("------ Score Board ------")
+  print(f"{name.title()}: {PlayerScore} | Computer: {ComputerScore}")
+  print("===============================")
+  print("")
+  if NumberOfRounds==3:
+    if PlayerScore==ComputerScore:
+      print("Game over.  Its a draw!!")
+    elif PlayerScore>ComputerScore:
+      print(f"Congrats {name.title()}, You won the game!!")
+    else:
+      print(f"L Bozo you lose. The computer won the game!! Better luck next time {name.title()}!")
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() == "y":
+      NumberOfRounds = 0
+    else:
+      break
